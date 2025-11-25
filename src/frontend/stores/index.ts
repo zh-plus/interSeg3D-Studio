b478f39be6501588b50073154f11b77e0a7d303e
Quit123 <2048751307@qq.com>
2025-11-25 10:48:31 +0800

    clean main branch for new project

--- src/frontend/stores/index.ts
+++
@@ -1,8 +0,0 @@
-// Export all stores for easy import
-export {usePointCloudStore} from './pointCloud.store';
-export {useAnnotationStore} from './annotation.store';
-export {useUiStore} from './ui.store';
-export {useApiStore} from './api.store';
-
-// Types
-export type {ObjectData} from './ui.store';
